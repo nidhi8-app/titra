492fbf763e6971d075331ece2af8f186ba376c4e
Nidhi Manoj <nidhimanoj021@gmail.com>
2025-12-19 13:15:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9675338345-3be7c",
+  "appId": "1:76638457168:web:2fe4a072e12a153149f055",
+  "apiKey": "AIzaSyDuRFseihsy_JEYfiYdAqd3YH4eKk_LRw0",
+  "authDomain": "studio-9675338345-3be7c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "76638457168"
+};
